99804b7b69a12f550ac05d32307b3a6c086d5c1c
Yishuo Wang <yishuo@ualberta.ca>
2016-04-19 21:45:53 -0600

    Running

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (0, 10, -10);
+		transform.position = player.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText, winText;
+
+	private Rigidbody rb;
+	private float moveHorizontal, moveVertical;
+	private Vector3 movement;
+	private int pickupCount, winCount;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		pickupCount = 0;
+		winCount = 0;
+		SetcountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * 10);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pickup")) {
+			other.gameObject.SetActive(false);
+			pickupCount++;
+			SetcountText ();
+		} else if (other.gameObject.CompareTag("PickupWin")) {
+			other.gameObject.SetActive(false);
+			winCount++;
+			winText.text = "You Win";
+		}
+	}
+
+	void SetcountText () {
+		countText.text = "Pickup Count :" + pickupCount.ToString ();
+	}
+}
